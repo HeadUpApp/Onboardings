deb954de53be2802ffad1a9d2b14eae0d47b67eb
softspheredev <98044068+softspheredev@users.noreply.github.com>
2025-09-27 13:29:08 +0200

    Remove redundant artifacts

--- OnboardingSPM/Sources/Onboardings/Extensions/Image+Bundle.swift
+++
@@ -1,8 +0,0 @@
-import SwiftUI
-
-extension Image {
-    /// Creates an Image from a resource in the SPM bundle
-    init(bundleResource name: String) {
-        self.init(name, bundle: Bundle.module)
-    }
-}
